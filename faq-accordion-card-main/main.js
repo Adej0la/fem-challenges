5fcfe1c1ba162ed08ae3abf9f42063d78bb22bab
Adejola <54010799+Adej0la@users.noreply.github.com>
2020-12-29 06:20:01 +0100

    Delete main.js
    It is unneeded at this time.

--- faq-accordion-card-main/main.js
+++
@@ -1,9 +0,0 @@
-const items = document.querySelector('.faq-content-item');
-
-
-items.forEach(item => {
-    item.addEventListener('click', () =>
-    {
-        document.body.classList.toggle('arrow-open');
-    })
-})
